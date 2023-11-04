fc1cf56e755e008740c86ff397f4a1ff764c2c59
chenbei97 <chenbei_electric@163.com>
2023-11-04 20:04:26 +0800

    update count_lines

---
+++ count_lines.bat
@@ -0,0 +1,1 @@
+python count_lines.py
---
+++ count_lines.py
@@ -0,0 +1,29 @@
+import os
+import time
+path = os.path.dirname(os.path.abspath(__file__))
+path1 = os.path.join(os.path.expanduser("~"), 'Desktop')+"/EOS"
+path2 = os.path.join(os.path.expanduser("~"), 'Desktop')+"/EOSI_Tests"
+# print(path1)
+total_lines = 0
+file_count = 0
+def f(path):
+    global total_lines,file_count
+    for filepath,dirnames,filenames in os.walk(path):
+        if filepath.count("cmake-build-debug") or filepath.count("bin") or filepath.count(".vscode")\
+            or filepath.count(".git") or filepath.count("opencv2") or filepath.count("toupcam") \
+            or filepath.count(".idea") or filepath.count("lib") or filepath.count("__pycache__"):
+            continue
+        
+        for filename in filenames:
+            
+            if filename.count(".h") or filename.count(".cpp") or filename.count(".py") or filename.count("CMakeLists.txt"):
+                    file_count += 1
+                    p = os.path.join(filepath,filename)
+                    print(file_count,p)
+                    count = len(open(p, 'r',encoding="utf-8").readlines())
+                    total_lines += count              
+
+if __name__ == '__main__':
+    f(path)
+    print("total lines = ",total_lines, "file count = ",file_count)
+    time.sleep(1)
